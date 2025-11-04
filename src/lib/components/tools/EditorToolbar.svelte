711bb031aa291cdc35a1655dcd395888a6d82009
root <root@CLD1L01.a-root.lan>
2025-11-05 03:51:24 +0700

    test db only 3

--- src/lib/components/tools/EditorToolbar.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { isEditing } from '$lib/stores.js';
-</script>
-
-{#if $isEditing}
-  {#await import('$lib/components/tools/EditorControls.svelte') then EditorToolbar}
-    <EditorToolbar.default on:cancel on:save />
-  {/await}
-{/if}
