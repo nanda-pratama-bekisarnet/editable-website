87a059a599ade391af3984c65a08fd91db29fe74
root <root@CLD1L01.a-root.lan>
2025-11-06 05:02:55 +0700

    fix upload button

--- src/routes/assets-test/+page.svelte
+++ src/routes/assets-test/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  let file = null;
+  let file = null;       // will store a single File
   let uploadedId = null;
 
   async function upload() {
-    if (!file) return;
+    if (!file) return alert("Please select a file!");
 
     const formData = new FormData();
     formData.append('file', file);
@@ -12,13 +12,17 @@
     const data = await res.json();
     uploadedId = data.asset_id;
   }
+
+  function handleFileChange(event) {
+    file = event.target.files[0]; // take the first selected file
+  }
 </script>
 
 <h1>Upload Image</h1>
-<input type="file" bind:files={file} />
+<input type="file" on:change={handleFileChange} />
 <button on:click={upload}>Upload</button>
 
 {#if uploadedId}
   <h2>Uploaded Image:</h2>
-  <img src={`/assets/images?id=${uploadedId}`} alt="Uploaded Image" width="300" />
+  <img src={`/api/assets?id=${uploadedId}`} alt="Uploaded Image" width="300" />
 {/if}
