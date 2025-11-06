6ebc78d9309bb0d4dd4c6ba6653f776d0e98ff41
root <root@CLD1L01.a-root.lan>
2025-11-06 22:24:15 +0700

    add image-test 4

--- src/routes/api/image-test/+server.ts
+++ src/routes/api/image-test/+server.ts
@@ -24,7 +24,7 @@ export const POST = async ({ request, platform }) => {
 		});
 
 		// Replace with your R2 public domain
-		const publicUrl = `https://<YOUR_R2_PUBLIC_URL>/${objectKey}`;
+		const publicUrl = `https://pub-d9e98b47ac19405d910faf87fc7b274a.r2.dev`;
 
 		// Insert into D1
 		await platform.env.DB.prepare(
